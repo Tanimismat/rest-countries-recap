65756f4cdc0a53488e4fbf4d7f3d3ad2ffb031c2
Tanimismat <tanim.ismat@gmail.com>
2022-03-14 22:49:29 +0600

    consoled 1 country

---
+++ js/countries.js
@@ -0,0 +1,23 @@
+const loadCountries = () => {
+    fetch(`https://restcountries.com/v3.1/all`)
+        .then(res => res.json())
+        .then(data => displayCountries(data));
+}
+
+const displayCountries = countries => {
+    // console.log(countries)
+    const countriesHTML = countries.map(country => getCountryHTML(country));
+    // console.log(countriesHTML)
+    console.log(countriesHTML[0]);
+}
+
+const getCountryHTML = country => {
+    return `
+        <div>
+            <h2>${country.name.common}</h2>
+        </div>
+    `
+}
+
+
+loadCountries();
